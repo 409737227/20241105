d62bbcdafb4bbafeafc9a5e493cdd954ab4893b9
409737227 <116100723+409737227@users.noreply.github.com>
2024-11-08 15:10:19 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,10 +2,13 @@ let font;  // 載入字型文字
 let points = [];  // 轉成點狀文字
 let r = 20; // 控制圓的半徑
 let rotationDirection = 0; // 控制旋轉方向: 0為正常，-1為上下翻轉，1為左右翻轉
+var sound1;
+var fc;
 
 // 在執行setup()前載入字型
 function preload() {  
     font = loadFont("fonts/PassionOne-Regular.ttf");
+    sound1 = loadSound("Karencici -  愛你但說不出口 (Official Music Video).mp3")
 }
 
 // 設定環境
@@ -13,6 +16,8 @@ function setup() {
     createCanvas(windowWidth, windowHeight); // 設定畫布的寬高
     angleMode(DEGREES);
     background("#bfd7ea"); // 背景顏色
+    analyzer = new p5.Amplitude();
+    analyzer.setInput(sound1)
     
     // 轉成文字圖檔
     points = font.textToPoints("TKUET", -300, 80, 200, {
@@ -29,6 +34,15 @@ function draw() {
     
     // 將文字點移動至畫布中心並根據旋轉方向旋轉
     translate(width / 2, height / 2);
+
+    if(sound1.isPlaying())
+    { //音樂有撥放時
+        fc = map(analyzer.getLevel(),0,1,0,100)
+    }
+    else
+    {//音樂沒撥放時
+        fc = map(mouseX,0,width,0,100)
+    }
 
     // 根據方向持續旋轉
     if (rotationDirection === 0) {
@@ -62,8 +76,8 @@ function draw() {
 
 // 繪製矩形和橢圓的圖案
 function drawPattern() {
-    let rect_width = 50 + mouseX / 10;
-    let bc_width = 50 + mouseY / 10;
+    let rect_width = 50 + fc;
+    let bc_width = 50 + fc;
     let sc_width = 25;
 
     for (let y = 0; y < height; y += rect_width) {
@@ -77,7 +91,7 @@ function drawPattern() {
             stroke("#9b5de5");
             ellipse(x, y, sc_width);
             stroke("#5390d9");
-            ellipse(x, y, sc_width - 25);
+            ellipse(x, y, sc_width - fc);
             stroke("#ffcfd2");
             rect(x + 15, y + 15, rect_width);
             rect(x - 15, y - 15, rect_width);
@@ -87,6 +101,12 @@ function drawPattern() {
 
 // 監聽滑鼠點擊事件
 function mousePressed() {
+    if(sound1.isPlaying()){
+        sound1.stop();
+    }else{
+        sound1.play();
+    }
+
     if (mouseY < height / 2) { // 上半部點擊
         rotationDirection = (rotationDirection === -1) ? 0 : -1; // 切換到上下翻轉或重設為正常
     } else { // 下半部點擊
@@ -99,3 +119,4 @@ function mousePressed() {
         rotationDirection = -1; // 設置為上下翻轉
     }
 }
+
